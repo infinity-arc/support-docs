54b260313e8ecddcf13b7adeebcb91895a5da816
C1I7F05D-C53545 <cliff.crerar@gmail.com>
2021-08-25 04:15:04 +0200

    Commit (#26)
    * delete azure pipeline
    
    * making git page docs
    
    * Create CNAME
    
    * Delete CNAME
    
    * minro
    
    * added dockerfile
    
    * cloud build
    
    * Create codeql-analysis.yml
    
    * exposing conatiner port
    
    * Create CNAME
    
    Co-authored-by: Cliff <cliff@infinityarc.nete>

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
+/***
+ * Description web server with express.js
+ */
+
 const port = process.env.PORT || 3000;
 const host = require('os').hostname();
-
 const express = require('express');
-
 const app = express();
-
-
 
 app
 .all('*',(req,res,next)=>{
     console.log(res.statusCode, '-', req.path);
     next();
 })
-.use('/',express.static('./Docs/.vuepress/dist'))
+.use('/',express.static('docs'))
+// .use('/',express.static('./src/.vuepress/dist'))
 .listen(port,()=>{
     console.log('Doc server running', host, ':',port);
 });
